f45d6979d0a8fcaf1547e36b30ba366897be39fc
Sergey Tretyak <serafim619@gmail.com>
2022-11-22 05:08:21 +0100

    test input rendering

--- src/components/exercise-form/link-input/link-input.test.ts
+++ src/components/exercise-form/link-input/link-input.test.ts
@@ -1,28 +1,36 @@
 import { createTestingPinia } from '@pinia/testing'
-import { mount } from '@vue/test-utils'
+import { mount, VueWrapper } from '@vue/test-utils'
 import ElementPlus from 'element-plus'
-import { describe, expect, it } from 'vitest'
+import { beforeEach, describe, expect, it } from 'vitest'
 
 import LinkInput from '@/components/exercise-form/link-input/LinkInput.vue'
 
 describe('LinkInput', () => {
-  it('should display label text', async () => {
-    const initialState = {
-      exerciseForm: {
-        form: {
-          link: ''
-        }
+  const initialState = {
+    exerciseForm: {
+      form: {
+        link: ''
       }
     }
-    const wrapper = mount(LinkInput, {
+  }
+  let wrapper: VueWrapper
+  const label = 'fakeLabel'
+
+  beforeEach(() => {
+    wrapper = mount(LinkInput, {
       global: {
         plugins: [createTestingPinia({ initialState }), ElementPlus]
       }
     })
-    const label = 'fakeLabel'
+  })
 
+  it('should display label text', async () => {
     await wrapper.setProps({ label })
 
     expect(wrapper.html()).toContain(label)
+  })
+
+  it('should render input field', () => {
+    expect(wrapper.find('input').exists())
   })
 })
