e02a8bf768f6616246a98a787d512bd8cbaf83d1
Siarhei Tratsiak <s.tratsiak@godeltech.com>
2022-11-24 21:19:15 +0100

    test interaction between form and store

--- src/components/exercise-form/exersice-form.test.ts
+++ src/components/exercise-form/exersice-form.test.ts
@@ -1,24 +1,34 @@
 import { createTestingPinia } from '@pinia/testing'
 import { mount } from '@vue/test-utils'
-import ElementPlus from 'element-plus'
+import ElementPlus, { ElForm } from 'element-plus'
 import { describe, expect, it, vi } from 'vitest'
 
 import ExerciseForm from '@/components/exercise-form/ExerciseForm.vue'
+import { useExerciseFormStore } from '@/stores/exercise-form/exerciseForm'
 
 describe('ExerciseForm', () => {
   const initialState = {
     exerciseForm: {
-      onValidate: vi.fn()
+      setValidate: vi.fn(),
+      isPropValid: {
+        name: true
+      }
     }
   }
 
   const wrapper = mount(ExerciseForm, {
     global: {
       plugins: [createTestingPinia({ initialState }), ElementPlus],
-      stubs: { DescriptionInput: true, SubmitButton: true }
+      stubs: {
+        DescriptionInput: true,
+        LinkInput: true,
+        NameInput: true,
+        SubmitButton: true
+      }
     }
   })
 
+  const store = useExerciseFormStore()
   const form = wrapper.find('form')
   const errorMessage = wrapper.find('.el-form-item__error')
 
@@ -28,5 +38,18 @@ describe('ExerciseForm', () => {
 
   it('should not render error message by default', () => {
     expect(errorMessage.exists()).toBe(false)
+  })
+
+  it('should set store validate value', () => {
+    expect(store.setValidate).toBeCalled()
+  })
+
+  it('should set prop value on validate', async () => {
+    const formComponent = wrapper.findComponent(ElForm)
+    await formComponent.trigger('validate', {
+      prop: 'name',
+      isValid: false
+    })
+    expect(store.isPropValid.name).toBe(false)
   })
 })
