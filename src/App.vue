aa9c17b6aad285b924cbdadffe0fc32bd9b7ef2d
Siarhei Tratsiak <s.tratsiak@godeltech.com>
2022-11-26 21:31:18 +0100

    create structurefor exercise list

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
 import ExerciseForm from '@/components/exercise-form/ExerciseForm.vue'
+import ExerciseList from '@/components/exercise-list/ExerciseList.vue'
 </script>
 
 <template>
+  <ExerciseList />
+
   <ExerciseForm />
 </template>
