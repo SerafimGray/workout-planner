3331fee2b18da7e4c6fb9ee481759e0f867e78c9
Sergey Tretyak <serafim619@gmail.com>
2022-11-26 14:05:27 +0100

    test exercise store add action

--- src/stores/exercise-form/exercise-form.test.ts
+++ src/stores/exercise-form/exercise-form.test.ts
@@ -2,20 +2,40 @@ import { setActivePinia, createPinia } from 'pinia'
 import { describe, beforeEach, it, vi, expect } from 'vitest'
 
 import { useExerciseFormStore } from '@/stores/exercise-form/exerciseForm'
+import 'fake-indexeddb/auto'
 
 describe('exerciseForm store', () => {
   beforeEach(() => {
     setActivePinia(createPinia())
   })
 
+  const validate = vi.fn(callback => callback(true))
+
+  vi.mock('@/services/db/db', () => {
+    class DBService {
+      addExercise() {
+        return new Promise(() => 1)
+      }
+    }
+
+    return { DBService }
+  })
+
   describe('setValidate', () => {
     it('should set validate', () => {
       const exerciseFormStore = useExerciseFormStore()
-      const validate = vi.fn()
 
       exerciseFormStore.setValidate(validate)
       exerciseFormStore.add()
 
+      expect(validate).toBeCalled()
+    })
+  })
+
+  describe('add', () => {
+    it('should add record to database', () => {
+      const exerciseFormStore = useExerciseFormStore()
+      exerciseFormStore.add()
       expect(validate).toBeCalled()
     })
   })
